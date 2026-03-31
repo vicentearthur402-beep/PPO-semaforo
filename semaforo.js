3db1e82212248f4fd1de81270924e6d3aa19b4e0
Arthur Vicente <vicentearthur402@gmail.com>
2026-03-31 15:01:37 -0300

    V2 ATUALIZADA DO SEMAFORO

--- semaforo.js
+++ semaforo.js
@@ -1,4 +1,4 @@
-const img = document.getElementById('img');
+/*const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 let colorIndex = 0;
 let intervalId = null
@@ -36,3 +36,46 @@ const turnOn = {
 }
 
 buttons.addEventListener('click', trafficLight);
+
+*/
+
+const semaforo_left = document.getElementById('img');
+const semaforo_right = document.getElementById('img2');
+
+const buttonLeft = document.getElementById('left');
+const buttonRight = document.getElementById('right');
+const auto = document.getElementById('automatic');
+
+let intervalId = null;
+
+buttonLeft.addEventListener('click', () => {
+    console.log("Clicou left");
+    turnOff.left();
+}); 
+
+buttonRight.addEventListener('click', () => {
+    console.log("click right")
+    turnOff.right();
+});
+
+const turnOff = {
+    left: () => {
+    semaforo_left.src = './img/amarelo.png';
+
+    setTimeout(() => {
+        semaforo_left.src = './img/vermelho.png';
+        semaforo_right.src = './img/verde.png';
+    }, 1000);
+    },
+    right: () => {
+    semaforo_right.src = './img/amarelo.png';
+
+    setTimeout(() => {
+            semaforo_left.src = './img/verde.png';
+            semaforo_right.src = './img/vermelho.png';
+    }, 1000);
+    }
+}
+
+
+
